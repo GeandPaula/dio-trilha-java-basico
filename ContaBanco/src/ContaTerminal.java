49fdeb78d10d43000388239eafcf9a341903f5fe
GeandPaula <geandpaula25@gmail.com>
2023-04-26 09:43:27 +0100

    Criacao classe terminal para insercao de dados do cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da conta:");
+        int conta = sc.nextInt();
+
+        System.out.println("Digite o numero da agência:");
+        String agencia = sc.next();
+
+        System.out.println("Digite seu nome:");
+        String nomeCliente = sc.next();
+
+        System.out.println("Insira o saldo:");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +
+                ", Obrigado por criar uma conta em nosso banco , sua agência é "
+                + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.s");
+
     }
 }
